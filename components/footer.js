7a1dc5638dc9ad8217e0670a45be79bec2a96645
Md Firoz Mia <mdfirozmia321@gmail.com>
2022-10-22 19:15:20 +0600

    complete header file and add footer file

---
+++ components/footer.js
@@ -0,0 +1,3 @@
+export default function footer() {
+  return <div className='text-3xl'>footer</div>;
+}
--- components/header.js
+++ components/header.js
@@ -1,5 +1,5 @@
 import { ImFacebook2, ImTwitter, ImYoutube } from 'react-icons/im';
-
+import Link from 'next/link';
 export default function header() {
   return (
     <div>
@@ -13,19 +13,27 @@ export default function header() {
             ></input>
           </div>
           <div className='shrink w-80 sm:order-2'>
-            <a className='font-blod uppercase text-3xl'>Design</a>
+            <Link href={'/'}>
+              <a className='font-blod uppercase text-3xl'>Design</a>
+            </Link>
           </div>
           <div className='w-96 order-3 flex justify-center'>
             <div className='flex gap-6'>
-              <a>
-                <ImFacebook2 color='#888888'></ImFacebook2>
-              </a>
-              <a>
-                <ImTwitter color='#888888'></ImTwitter>
-              </a>
-              <a>
-                <ImYoutube color='#888888'></ImYoutube>
-              </a>
+              <Link href={'/'}>
+                <a>
+                  <ImFacebook2 color='#888888'></ImFacebook2>
+                </a>
+              </Link>
+              <Link href={'/'}>
+                <a>
+                  <ImTwitter color='#888888'></ImTwitter>
+                </a>
+              </Link>
+              <Link href={'/'}>
+                <a>
+                  <ImYoutube color='#888888'></ImYoutube>
+                </a>
+              </Link>
             </div>
           </div>
         </div>
